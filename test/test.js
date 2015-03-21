f154289eb331d9d734a3e1b5deb9cc96ecb108ba
Rasmus Eskola <fruitiex@gmail.com>
2015-03-21 11:47:14 +0200

    test also other required backend functions

--- test/test.js
+++ test/test.js
@@ -11,4 +11,13 @@ describe('plugin module', function() {
     it('should export a init function', function() {
         plugin.init.should.be.ok.and.should.be.a.Function;
     });
+    it('should export a search function', function() {
+        plugin.search.should.be.ok.and.should.be.a.Function;
+    });
+    it('should export an isPrepared function', function() {
+        plugin.isPrepared.should.be.ok.and.should.be.a.Function;
+    });
+    it('should export a prepareSong function', function() {
+        plugin.prepareSong.should.be.ok.and.should.be.a.Function;
+    });
 });
